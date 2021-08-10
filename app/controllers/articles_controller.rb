f73698588c354a667bd4b29289a245427fe0fdf9
tarosuke777 <felis.silvestris.catus.7777@gmail.com>
2021-08-10 20:38:56 +0900

    modify

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,11 +2,11 @@ class ArticlesController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   def index
-    @articles = Article.includes(:user).all.order(created_at: :desc)
+    @articles = Article.includes(:user).all.order(created_at: :desc).limit(20)
   end
 
   def following
-    @articles = Article.includes(:user).where(user_id: current_user.following).order(created_at: :desc)
+    @articles = Article.includes(:user).where(user_id: current_user.following).order(created_at: :desc).limit(20)
   end
 
   def create
