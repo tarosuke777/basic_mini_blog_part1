09e083491a52ebd1dc8d2a04b34a9e3e5ebd735b
tarosuke777 <felis.silvestris.catus.7777@gmail.com>
2021-08-11 16:24:37 +0900

    modify

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
+  devise_scope :user do
+    get '/users/sign_in', to: 'users/sessions#new', as: :new_user_session
+    post '/users/sign_in', to: 'users/sessions#create', as: :user_session
+    delete '/users/sign_out', to: 'users/sessions#destroy', as: :destroy_user_session
+    get '/users/sign_up', to: 'users/registrations#new', as: :new_user_registration
+    post '/users/sign_up', to: 'users/registrations#create', as: :user_registration
+    get '/users', to: 'users/registrations#edit', as: :edit_user_registration
+    put '/users', to: 'users/registrations#update', as: :update_user_registration
+    delete '/users', to: 'users/registrations#destroy', as: :destroy_user_registration
+  end
 
   resources :users, only: %i[show] 
   resources :relationships, only: %i[create destroy]
