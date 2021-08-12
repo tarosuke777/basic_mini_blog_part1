b8660b7c3b581edc3d5e8347409a35155f577dac
tarosuke777 <felis.silvestris.catus.7777@gmail.com>
2021-08-12 11:35:02 +0900

    add basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+    before_action :basic
+
+    private
+    
+    def basic
+        if Rails.env.production?
+            authenticate_or_request_with_http_basic do |name, password|
+                name == ENV['BASIC_AUTH_NAME'] && password == ENV['BASIC_AUTH_PASSWORD']
+            end
+        end
+    end
 end
