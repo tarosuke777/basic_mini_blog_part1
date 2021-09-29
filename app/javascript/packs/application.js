dcdeafefdfacefdaa530a5fb2938123dea309f97
tarosuke777 <felis.silvestris.catus.7777@gmail.com>
2021-09-29 22:46:42 +0900

    mod bootstrap 4 to 5

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,12 +7,12 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "bootstrap"
-import "@fortawesome/fontawesome-free/js/all"
-import "../src/application.scss"
-import 'bootstrap-icons/font/bootstrap-icons.css'
-import bsCustomFileInput from 'bs-custom-file-input';
-import "jquery"
+//import "bootstrap"
+//import "@fortawesome/fontawesome-free/js/all"
+//import "../src/application.scss"
+//import 'bootstrap-icons/font/bootstrap-icons.css'
+//import bsCustomFileInput from 'bs-custom-file-input';
+//import "jquery"
 
 Rails.start()
 Turbolinks.start()
