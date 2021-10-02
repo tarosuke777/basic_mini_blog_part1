c919a234136a25aa1b2fa49bccfd568cadcb47f7
tarosuke777 <felis.silvestris.catus.7777@gmail.com>
2021-10-02 22:59:12 +0900

    mod application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,22 +1,14 @@
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.      
+// that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
-// import "bootstrap/scss/bootstrap.scss"
-// import "bootswatch/dist/spacelab/bootstrap.css"
-// import "bootswatch/dist/cerulean/bootstrap.css"
 import "@fortawesome/fontawesome-free/js/all"
-// import "@fortawesome/fontawesome-free/scss/fontawesome.scss"
-// import "@fortawesome/fontawesome-free/scss/brands.scss"
-// import "@fortawesome/fontawesome-free/scss/regular.scss"
-// import "@fortawesome/fontawesome-free/scss/solid.scss"
-// import "@fortawesome/fontawesome-free/scss/v4-shims.scss"
 
 Rails.start()
 Turbolinks.start()
